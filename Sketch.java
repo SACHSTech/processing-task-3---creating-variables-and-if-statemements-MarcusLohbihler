2397d6ae2fcceacd5e4dccaa64e72f177a96b3eb
Marcus Lohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-03-06 00:25:48 +0000

    finished code with comments

--- Sketch.java
+++ Sketch.java
@@ -15,8 +15,19 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
+
+    //code assigns shapes random X and Y values. Screen is divided into quadrants. Colour changes depending on which quadrant shape is in. Red = top left. Green = top right. Blue = bottom left. Yellow = bottom right
+  
+    //declaring variables to determine X and Y of shapes
+    float randomX = random(0, 400);
+    float randomY = random(0, 400);
+    float randomX2 = random(0, 400);
+    float randomY2 = random(0, 400);
+    float randomX3 = random(0, 400);
+    float randomY3 = random(0, 400);
+    float objectSize = random(25, 250);
   public void setup() {
-    background(210, 255, 173);
+    background(255);
   }
 
   /**
@@ -24,13 +35,56 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  
+    //Position and Colour (Square)
+    if(randomX < 200 && randomY < 200){
+      fill(255, 0, 0);
+    } else if (randomX > 200 && randomY < 200){
+      fill(0, 255, 0);
+    } else if (randomX < 200 && randomY > 200){
+      fill(0,0, 255);
+    }else if (randomX > 200 && randomY > 200){
+      fill(255, 255, 0);
+    }
+    
+    rect(randomX, randomY, objectSize, objectSize);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    //Position and Colour (Circle)
+    if(randomX2 < 200 && randomY2 < 200){
+      fill(255, 0, 0);
+    } else if (randomX2 > 200 && randomY2 < 200){
+      fill(0, 255, 0);
+    } else if (randomX2 < 200 && randomY2 > 200){
+      fill(0,0, 255);
+    }else if (randomX2 > 200 && randomY2 > 200){
+      fill(255, 255, 0);
+    }
+    ellipse(randomX2, randomY2, objectSize, objectSize);
+
+    //Position and Colour (Triangle)
+    if(randomX3 < 200 && randomY3 < 200){
+      fill(255, 0, 0);
+    } else if (randomX3 > 200 && randomY3 < 200){
+      fill(0, 255, 0);
+    } else if (randomX3 < 200 && randomY3 > 200){
+      fill(0,0, 255);
+    }else if (randomX3 > 200 && randomY3 > 200){
+      fill(255, 255, 0);
+    }
+    triangle(randomX, randomY, randomX2, randomY2, randomX3, randomY3);
+
+    //Lines to show quadrants
+    fill(0);
+    line(200, 0, 200, 400);
+
+    fill(0);
+    line(0, 200, 400, 200);
+
+    
+
+    
+    
   
   // define other methods down here.
+}
 }
